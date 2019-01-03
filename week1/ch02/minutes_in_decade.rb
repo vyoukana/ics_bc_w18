92a97f63ed021041abd29e5b299466aa624b0786
vyoukana <vyoukana@berkeley.edu>
2019-01-03 01:52:39 -0800

    complete

--- week1/ch02/minutes_in_decade.rb
+++ week1/ch02/minutes_in_decade.rb
@@ -3,3 +3,10 @@
 
 
 ### Your Code Here ###
+years_in_decade = 10
+leap_years = 3
+years_to_min = (7 * 365*24*60)
+leap_to_min = (leap_years*365.25*24.00*60.00)
+min_in_decade = years_to_min + leap_to_min
+
+puts "There are " + (min_in_decade).to_s + " minutes in a decade"
