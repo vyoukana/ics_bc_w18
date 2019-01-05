ac368fec68ec54e9f63c29ee00ee0068d3cd4c9f
vyoukana <vyoukana@berkeley.edu>
2019-01-05 02:24:00 -0800

    grandma complete

--- week2/ch07/deaf_grandma.rb
+++ week2/ch07/deaf_grandma.rb
@@ -1,7 +1,18 @@
 puts 'COME GIVE GRANNY A KISS!'
 
-while () # Fill in the while condition
+while true # Fill in the while condition
   ### Your Code Here ###
+  you_say = gets.chomp
+  year = rand(51)
+  if you_say == you_say.upcase
+    puts "NO, NOT SINCE " + (year+1920).to_s + "!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+    end
+  if you_say == "BYE"
+     puts 'OK SONNY, TALK AGAIN SOON'
+     break
+   end
 end
 
-puts 'OK SONNY, TALK AGAIN SOON'
+#puts 'OK SONNY, TALK AGAIN SOON'
